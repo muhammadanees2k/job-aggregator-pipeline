1fadbcdd1a1b2846e13d7586e9f5e246055834f0
Muhammad Anees <muhammadanees2k@gmail.com>
2026-04-14 13:09:27 +0500

    feat: build rest endpoints and responsive grid ui for job listings

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from rest_framework.mixins import ListModelMixin
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Job
+from .serializers import JobSerializer
 
-# Create your views here.
+class JobViewSet(ListModelMixin, GenericViewSet):
+    queryset = Job.objects.filter(is_active=True).order_by("-last_seen")
+    serializer_class = JobSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["company"]
+    search_fields = ["job_title"]
+
+def job_board(request):
+    company_filter = request.GET.get("company")
+    jobs = Job.objects.filter(is_active=True).order_by("-last_seen")
+    
+    if company_filter:
+        jobs = jobs.filter(company=company_filter)
+
+    paginator = Paginator(jobs, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "jobs/job_board.html", {
+        "page_obj": page_obj,
+        "company_filter": company_filter
+    })
